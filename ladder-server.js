ec77ecd34ff32c8cf4fbe0c1c37f0896dbc4cbe4
Kieran Dunn <kpdunn@gmail.com>
2013-08-07 21:43:40 +0100

    try a new port env

--- ladder-server.js
+++ ladder-server.js
@@ -21,7 +21,11 @@ app.configure( 'production', function () {
   app.use( express.errorHandler() ); 
 });
 
-app.listen(3000);
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+	console.log("App listening on " + port);
+});
 /* console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 */
 console.log("Started now.");
